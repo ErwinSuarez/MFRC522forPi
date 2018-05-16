2bd12fb252a71b98e1b305b27fad2a93f9ecbdf7
ErwinSuarez <erwinsuarez2003@yahoo.com>
2018-05-16 13:37:54 +0100

    Create eventDBv2.sql
    Database Schema

---
+++ eventDBv2.sql
@@ -0,0 +1,49 @@
+CREATE SCHEMA IF NOT EXSITS `eventDBv2`;
+
+USE eventDBv2;
+
+DROP TABLE I EXISTS `availability`;
+DROP TABLE I EXISTS `records`;
+DROP TABLE I EXISTS `emps`;
+DROP TABLE I EXISTS `deps`;
+DROP TABLE I EXISTS `rooms`;
+
+CREATE table deps(
+  depID INTEGER AUTO_INCREMENT PRIMARY KEY,
+  dep_code char(5),
+  description char(30)
+);
+
+CREATE table rooms(
+  roomID INTEGER AUTO_INCREMENT PRIMARY KEY,
+  description char(20)
+);
+
+CREATE table emps(
+  empID INTEGER AUTO_INCREMENT PRIMARY KEY,
+  uid char(30),
+  depID INTEGER,
+  CONSTRAINT FK_EmpDepID FOREIGN KEY(depID) REFERENCES deps(depID)
+);
+
+CREATE table records(
+  recordID INTEGER AUTO_INCREMENT PRIMARY KEY,
+  roomID INTEGER,
+  uid char(30),
+  timecheck timestamp,
+  depID INTEGER,
+  flag bool,
+  auth bool,
+  CONSTRAINT FK_RecordRoomID FOREIGN KEY(roomID) REFERENCES rooms(roomID)
+);
+
+CREATE table availalbity(
+  ID INTEGER AUTO_INCREMENT PRIMARY KEY,
+  roomID INTEGER,
+  hca bool,
+  hsk bool,
+  timecheck timestamp,
+  flag bool,
+  roomStatus bool,
+  CONSTRAINT FK_AvRoomID FOREIGN KEY(roomID) REFERENCES rooms(roomID)
+);
