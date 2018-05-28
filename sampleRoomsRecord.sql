0efb7e3f126a9bb5939823ed1f53eb2900980559
ErwinSuarez <erwinsuarez2003@yahoo.com>
2018-05-28 14:47:11 +0100

    Insert a sample rooms record
    Insert a sample record to rooms

---
+++ sampleRoomsRecord.sql
@@ -0,0 +1,8 @@
+USE eventDBv2;
+
+INSERT INTO rooms(description) VALUES('S201');
+INSERT INTO rooms(description) VALUES('S206');
+INSERT INTO rooms(description) VALUES('S302');
+INSERT INTO rooms(description) VALUES('T300');
+INSERT INTO rooms(description) VALUES('T302');
+INSERT INTO rooms(description) VALUES('T200');
