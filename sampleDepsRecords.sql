5772bfec76f5dbf6a85bb7c4f85a06ddec5d3663
ErwinSuarez <erwinsuarez2003@yahoo.com>
2018-05-28 14:44:10 +0100

    Insert deps record
    Insert a sample deps record to eventDBv2

---
+++ sampleDepsRecords.sql
@@ -0,0 +1,7 @@
+USE eventDBv2;
+
+INSERT INTO deps(dep_code,description) VALUES('HCA','Hospital Career');
+INSERT INTO deps(dep_code,description) VALUES('HK','House Keeper');
+INSERT INTO deps(dep_code,description) VALUES('MP','Medical Practitioner');
+INSERT INTO deps(dep_code,description) VALUES('NR','Nurse');
+INSERT INTO deps(dep_code,description) VALUES('CS','Catering Staff');
