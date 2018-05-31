847aff0c43f7dece2f48453fc42a40ded3a6015d
ErwinSuarez <erwinsuarez2003@yahoo.com>
2018-05-31 16:28:21 +0100

    Create masterDBv2.sql
    Created masterDB for master Pi

---
+++ masterDBv2.sql
@@ -0,0 +1,49 @@
+CREATE SCHEMA IF NOT EXISTS `eventDBv2`;
+
+USE eventDBv2;
+
+DROP TABLE IF EXISTS `availability`;
+DROP TABLE IF EXISTS `records`;
+DROP TABLE IF EXISTS `emps`;
+DROP TABLE IF EXISTS `deps`;
+DROP TABLE IF EXISTS `rooms`;
+
+CREATE table deps(
+  depID INTEGER AUTO_INCREMENT PRIMARY KEY,
+  dep_code varchar(10),
+  description varchar(30)
+);
+
+CREATE table rooms(
+  roomID INTEGER AUTO_INCREMENT PRIMARY KEY,
+  description varchar(15)
+);
+
+CREATE table emps(
+  empID INTEGER AUTO_INCREMENT PRIMARY KEY,
+  uid varchar(50),
+  depID INTEGER,
+  CONSTRAINT FK_EmpDepID FOREIGN KEY(depID) REFERENCES deps(depID)
+);
+
+CREATE table records(
+  recordID INTEGER AUTO_INCREMENT PRIMARY KEY,
+  roomID INTEGER,
+  uid varchar(50),
+  timecheck timestamp,
+  depID INTEGER,
+  flag bool,
+  auth bool,
+  CONSTRAINT FK_RecordRoomID FOREIGN KEY(roomID) REFERENCES rooms(roomID)
+);
+
+CREATE table availability(
+  ID INTEGER AUTO_INCREMENT PRIMARY KEY,
+  roomID INTEGER,
+  HCA bool,
+  HK bool,
+  timecheck timestamp,
+  flag bool,
+  roomStatus bool,
+  CONSTRAINT FK_AvRoomID FOREIGN KEY(roomID) REFERENCES rooms(roomID)
+);
